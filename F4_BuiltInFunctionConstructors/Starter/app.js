8efe1226004369345bb961ab276fec9463e5ec2a
thedancollins <thedancollins@hotmail.com>
2019-03-19 12:49:04 -0500

    Section 6 Video 60: Conceptual Aside: Built-in Function Constructors

---
+++ F4_BuiltInFunctionConstructors/Starter/app.js
@@ -0,0 +1,11 @@
+String.prototype.isLngthGreaterThan = function (limit) {
+    return this.length > limit;
+}
+console.log("John".isLngthGreaterThan(3));
+
+Number.prototype.isPositive = function () {
+    return this > 0;
+}
+//3.isPositive(); throws an error because 3 is not automatically "boxed" by the parser
+var a = new Number(3);
+console.log(a.isPositive());
