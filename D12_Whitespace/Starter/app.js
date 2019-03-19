cf50454bb53ffdef8caee01d0945e1cac2b41f74
thedancollins <thedancollins@hotmail.com>
2019-03-19 06:12:43 -0500

    Section 4 Video 43: Framework Aside: Whitespace

---
+++ D12_Whitespace/Starter/app.js
@@ -0,0 +1,25 @@
+var firstname, lastname, language;
+
+var person = {
+    firstname: "Dan",
+    lastname: "Collins",
+    language: "en"
+}
+
+console.log(person);
+
+
+var
+    //this is not different
+    firstname2,
+    //Javascript allows liberal whitespace
+    lastname2, language2;
+
+var person2 = {
+    firstname: "Dan",
+    //You can even do it in object literals
+    lastname: "Collins",
+    language: "en"
+}
+
+console.log(person2);
