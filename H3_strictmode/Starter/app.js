f1f2c5a05c66a3ac96f585c81cf57b4d3365b339
thedancollins <thedancollins@hotmail.com>
2019-03-19 13:38:21 -0500

    Section 7 Video 67: Strict Mode

---
+++ H3_strictmode/Starter/app.js
@@ -0,0 +1,14 @@
+//"use strict"; <- this will run without this line.  But you can uncomment it and it will error out
+
+function logNewPerson() {
+    "use strict";
+    var person2;
+    persom2 = {};
+    console.log(persom2);
+
+}
+
+var person;
+persom = {};
+console.log(persom);
+logNewPerson();
